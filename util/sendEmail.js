21e93d3a52874c227a7dcfa39e51734088dc01e8
Khandoker Tahmid Sami <prinon96@gmail.com>
2026-05-08 15:05:34 +0200

    forgot password and sending email is running

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,16 +1,56 @@
-const { Resend } = require("resend");
+// const { Resend } = require("resend");
+
+// const sendEmail = async (to, subject, html) => {
+//   const resend = new Resend(process.env.RESEND_API_KEY);
+//   const { error } = await resend.emails.send({
+//     from: "Facehook App <onboarding@resend.dev>",
+//     to,
+//     subject,
+//     html,
+//   });
+
+//   if (error) {
+//     throw new Error(error.message || error.name || "Failed to send email");
+//   }
+// };
+
+// module.exports = sendEmail;
+
+const nodemailer = require("nodemailer");
+
+const getGmailConfig = () => {
+  const user = process.env.GMAIL_USER;
+  const pass = process.env.GMAIL_APP_PASSWORD;
+
+  if (!user || !pass) {
+    throw new Error("Email is not configured. Set GMAIL_USER and GMAIL_APP_PASSWORD in .env.");
+  }
+
+  return { user, pass };
+};
 
 const sendEmail = async (to, subject, html) => {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-  const { error } = await resend.emails.send({
-    from: "Facehook App <onboarding@resend.dev>",
-    to,
-    subject,
-    html,
+  const { user, pass } = getGmailConfig();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user,
+      pass,
+    },
   });
 
-  if (error) {
-    throw new Error(error.message || error.name || "Failed to send email");
+  try {
+    await transporter.sendMail({
+      from: `"Facehook App" <${user}>`,
+      to,
+      subject,
+      html,
+    });
+  } catch (err) {
+    throw new Error(`Email delivery failed: ${err.message}`);
   }
 };
 
